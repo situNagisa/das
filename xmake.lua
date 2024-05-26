c8d546237b59dbec3fcda9da68f2ea959a4feeff
situNagisa <1300296933@qq.com>
2024-05-27 02:48:01 +0800

    1

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,8 @@ set_optimize("none")
 --set_optimize("fast")
 set_warnings("all")
 
+--include
+
 add_includedirs("./include")
 add_includedirs("./extern/imgui/include")
 add_includedirs("./extern/imgui/include/backend")
@@ -15,6 +17,13 @@ option("nagisa_root")
 	set_default(os.getenv("NGS_ROOT"))
 	set_showmenu(true)
 option_end()
+
+option("visual_pcie")
+	set_default(false)
+	set_showmenu(true)
+option_end()
+
+--depend
 
 if "$(nagisa_root)" then 
 	add_includedirs("$(nagisa_root)/include")
@@ -41,13 +50,29 @@ end
 
 add_cxflags("cl::/Zc:preprocessor", {force = true})
 
-includes("extern/imgui","extern/implot","extern/pcie6920")
+includes("extern/imgui","extern/implot")
+
+if "$(visual_pcie)" then
+	includes("extern/pcie6920")
+end
 
 
 target("das")
 	set_kind("binary")
 
-	add_deps("imgui","implot","pcie6920")
+	add_deps("imgui","implot")
+
+	if "$(visual_pcie)" then
+		add_deps("pcie6920")
+	else
+		add_linkdirs("./lib")
+		if is_arch("x64") then
+			add_links("pcie6920_250m_64bits_api")
+		end
+		if is_arch("x86") then
+			add_links("pcie6920_250m_32bits_api")
+		end
+	end
 
 	add_packages("vcpkg::glfw3","opengl")
 	if is_plat("windows") then
